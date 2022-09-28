5f5ee72a67b0aacd2af994b64dcf495ba5191598
Rajendra <itsrajendrapatel@gmail.com>
2022-09-28 18:42:05 +0530

    Part 2 of Day-2 has been pushed

--- all_files/src/components/Home/UserExperience.jsx
+++ all_files/src/components/Home/UserExperience.jsx
@@ -1,5 +1,6 @@
 import { Box, Heading } from "@chakra-ui/react";
 import React from "react";
+import SimpleSlider from "./ReactSliders";
 
 export default function UserExperience() {
   return (
@@ -8,6 +9,7 @@ export default function UserExperience() {
         <Heading as="h2" size="3xl" noOfLines={2} w={"50%"} textAlign="left">
           What our amazing users have to say
         </Heading>
+        <SimpleSlider />
       </Box>
     </>
   );
